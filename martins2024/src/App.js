b152fea078a2f0e857af324ef5ac1bcbfe3ed342
will <wshirey@woh.rr.com>
2024-01-29 17:12:37 -0500

    first commit

---
+++ martins2024/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import logo from "./logo.jpg";
+import video from "./assets/IMG_1650.MOV";
+import "./App.css";
+/*import { useState } from "react";*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ReactPlayer url={video} width="100%" height="100%" controls={true} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+/*function App() {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="App">
+      <button
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
+        Hover over me!
+      </button>
+      {isShown && (
+        <div>
+          I'll appear when you hover over the button.
+        </div>
+      )}
+    </div>
+  );
+}*/
+
+export default App;
